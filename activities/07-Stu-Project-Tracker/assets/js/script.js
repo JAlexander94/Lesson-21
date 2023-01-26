7954c19f39c00980dfd2b431889092d6357dd337
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-26 21:37:49 +0000

    simplified the dynamic clock function

--- activities/07-Stu-Project-Tracker/assets/js/script.js
+++ activities/07-Stu-Project-Tracker/assets/js/script.js
@@ -1,9 +1,5 @@
 //working clock that sends the text to #date-time h2 in the header
-function updateMoment(){
+setInterval(function(){
     var today = moment()
     $("#date-time").text(today.format("DD MMM YYYY")+" at "+today.format("hh:mm:ss a"));
-}
-updateMoment()
-setInterval(function(){
-    updateMoment()
 },1000)
