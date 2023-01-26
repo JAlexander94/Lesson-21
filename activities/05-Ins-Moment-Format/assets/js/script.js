ee83429c3d7601b1d3335dcc36ffc6f09dc4a0c8
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-26 20:11:23 +0000

    completed exercise 6

--- activities/05-Ins-Moment-Format/assets/js/script.js
+++ activities/05-Ins-Moment-Format/assets/js/script.js
@@ -1,7 +1,7 @@
 // Use Moment.js to format the following variables:
 // 1. What is today's date in the following format: Jan 1st, 1999?
 var today = moment();
-$("#1a").text(today.format("D MMM YYYY"));
+$("#1a").text(today.format("D MMMM YYYY"));
 
 // 2. What is the day of the week today?
 var dayWeek = today.format("[Today is] dddd")
